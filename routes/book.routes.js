93c2d3f17ef15e2d022b99f78e66713629dd5320
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-08-03 08:17:59 +0200

    CRUD Create new book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 const Book = require("../models/Book.model");
 
 
-
 router.get('/books', (req, res, next) => {
     Book.find()
         .then( allTheBooksFromDB => {
@@ -16,8 +15,26 @@ router.get('/books', (req, res, next) => {
 });
 
 
-router.get('/books/:bookId', (req, res, next) => {
+router.get('/books/create', (req, res, next) => {
+    res.render("book-create");
+});
 
+
+router.post('/books/create', (req, res, next) => {
+    const { title, author, description, rating } = req.body;
+    Book.create( { title, author, description, rating } )
+        .then( bookFromDB => {
+            console.log(`New book created: ${bookFromDB.title}.`);
+            res.redirect('/books');
+        })
+        .catch( error => {
+            console.log('Error saving book in DB: ', error);
+            next(error);
+        });
+});
+
+
+router.get('/books/:bookId', (req, res, next) => {
     const { bookId } = req.params;
 
     Book.findById(bookId)
